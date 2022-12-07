651f76bbb3590a9f10b8048e2ec8c71ab983b815
Sabita Neupane <nsabita.sasa@gmail.com>
2022-12-07 17:26:41 +0545

    add load more button

--- src/Component/ListBeerDetails/ListAllBeers/index.tsx
+++ src/Component/ListBeerDetails/ListAllBeers/index.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import BeerCardView from "../BeerCardView";
 import { allBeerList } from "../../../Mock/allBeerList";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 
 const ListAllBeers = () => {
   return (
     <>
       <BeerCardView beerData={allBeerList} />
+      <p className="load-more-button">
+        <span> Load More </span>
+        <FontAwesomeIcon icon={faChevronDown} />
+      </p>
     </>
   );
 };
