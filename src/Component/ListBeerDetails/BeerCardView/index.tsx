0afbd73be5cab174eac996fea7d2eebff30e8747
Sabita Neupane <nsabita.sasa@gmail.com>
2022-12-09 19:06:56 +0545

    Show ingredients tooltip on image hover (#4)
    * Show ingredients tooltip on image hover

--- src/Component/ListBeerDetails/BeerCardView/index.tsx
+++ src/Component/ListBeerDetails/BeerCardView/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, Row, Col } from "react-bootstrap";
+import { Card, Row, Col, OverlayTrigger, Tooltip } from "react-bootstrap";
 import beerImg from "../../../Images/beer-img.png";
 
 interface IPros {
@@ -9,6 +9,77 @@ interface IPros {
 const BeerCardView = (props: IPros) => {
   const { beerData } = props;
 
+  const getIngredientsDetails = (beer: any) => {
+    let ingredientsDetails: any;
+    let ingredientsList: any = {};
+
+    if (beer && beer.ingredients) {
+      ingredientsDetails = Object.keys(beer.ingredients);
+      for (let i = 0; i <= ingredientsDetails.length - 1; i++) {
+        ingredientsList = {
+          ...ingredientsList,
+          [ingredientsDetails[i]]: [],
+        };
+
+        const ingredientsType = beer.ingredients[ingredientsDetails[i]];
+        if (Array.isArray(ingredientsType)) {
+          ingredientsType.map((e: any) => {
+            if (!ingredientsList[ingredientsDetails[i]].includes(e.name)) {
+              ingredientsList[ingredientsDetails[i]].push(e.name);
+            }
+          });
+        } else {
+          // for string
+          ingredientsList[ingredientsDetails[i]].push(ingredientsType);
+        }
+      }
+    }
+    return ingredientsList;
+  };
+
+  const showIngredientDetails = (ingredients: any) => {
+    let list = ingredients && Object.keys(ingredients);
+    let data: string = "";
+
+    if (list) {
+      for (let i = 0; i <= list.length - 1; i++) {
+        data = data + "\n" + `"${list[i]}": ${ingredients[list[i]].toString()}`;
+      }
+    }
+
+    return (
+      <>
+        Ingredients: <br /> {data}
+      </>
+    );
+  };
+
+  const cardImgContent = (beer: any) => {
+    return (
+      <Card.Img
+        variant="bottom"
+        src={beer.image_url ? beer.image_url : beerImg}
+        className={`beers-img ${beer.ingredients && "cursor-pointer"}`}
+      />
+    );
+  };
+  const toolTipContent = (beer: any) => {
+    const ingredients = getIngredientsDetails(beer);
+    return (
+      <OverlayTrigger
+        placement="top"
+        delay={{ show: 250, hide: 400 }}
+        overlay={
+          <Tooltip id={`tooltip-top`}>
+            {ingredients && showIngredientDetails(ingredients)}
+          </Tooltip>
+        }
+      >
+        {cardImgContent(beer)}
+      </OverlayTrigger>
+    );
+  };
+
   return (
     <>
       {beerData &&
@@ -16,13 +87,12 @@ const BeerCardView = (props: IPros) => {
           return (
             <Card className="beer-card m-2" key={beer.id ? beer.id : index}>
               <Row>
-                <Col xs={2}>
-                  <Card.Img
-                    variant="bottom"
-                    src={beer.image_url ? beer.image_url : beerImg}
-                    className="beers-img"
-                  />
+                <Col xs={2} className="card-img-section">
+                  {beer.ingredients
+                    ? toolTipContent(beer)
+                    : cardImgContent(beer)}
                 </Col>
+
                 <Col className="beer-card-text-content">
                   <Card.Body>
                     <Card.Title>{beer.name}</Card.Title>
