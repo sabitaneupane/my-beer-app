837ef26f1a48d28a03db40040b313e1086397dd1
Sabita Neupane <nsabita.sasa@gmail.com>
2023-09-20 21:37:24 +0545

    Fix UI and rename folder  (#9)
    * rename folder

--- src/Component/ListBeerDetails/ListAllBeers/index.tsx
+++ src/Component/ListBeerDetails/ListAllBeers/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import BeerCardView from "../BeerCardView";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
@@ -6,6 +6,7 @@ import { getAllBeerList } from "../../../API";
 import Error from "../../../Utils/Error";
 import Loading from "../../../Utils/Loading";
 import { Beers } from "../../../Types/beers";
+import { BeersQuery } from "../../../Types";
 import EmptyState from "../../../Utils/EmptyState";
 
 const ListAllBeers = () => {
@@ -18,15 +19,12 @@ const ListAllBeers = () => {
   const [initialLoad, setInitialLoad] = useState<boolean>(true);
 
   const perPageValue = 10;
-
   const fetchData = useCallback(
     async (pageNo: number, isFetchingMore: boolean) => {
+      setIsLoading(true);
       try {
         const data = await getAllBeerList(queryBuilder(pageNo));
-        setHasMoreData(data.length >= perPageValue);
-        setAllBeerList((prevData) =>
-          isFetchingMore ? [...prevData, ...data] : data
-        );
+        setAllBeerList(data);
       } catch (error) {
         setIsError(true);
       } finally {
@@ -45,16 +43,35 @@ const ListAllBeers = () => {
     }
   }, [pageNum, fetchData, initialLoad]);
 
-  const queryBuilder = (pageNo: number) => ({
-    page: pageNo,
-    perPage: perPageValue,
-  });
+  const queryBuilder = (pageNo: number) => {
+    const query: BeersQuery = {
+      page: pageNo,
+      perPage: perPageValue,
+    };
+
+    return query;
+  };
 
   const loadMoreDetails = () => {
     const nextPageNum = pageNum + 1;
     setPageNum(nextPageNum);
+    fetchMoreBeerDetails(nextPageNum);
+  };
+
+  const fetchMoreBeerDetails = async (pageNo: number) => {
     setIsLoadMoreLoading(true);
-    fetchData(nextPageNum, true);
+    let newData: any = [];
+    newData = newData.concat(allBeerList);
+
+    try {
+      const data = await getAllBeerList(queryBuilder(pageNo));
+      setHasMoreData(data.length >= perPageValue);
+      setAllBeerList(newData.concat(data));
+    } catch (error) {
+      console.log("error:", error);
+    }
+
+    setIsLoadMoreLoading(false);
   };
 
   if (isLoading) {
@@ -76,12 +93,13 @@ const ListAllBeers = () => {
         isLoadMoreLoading ? (
           <p className="text-align-center load-more-button mt-4">Loading ...</p>
         ) : (
-          <p className="load-more-button" onClick={loadMoreDetails}>
+          <p className="load-more-button" onClick={() => loadMoreDetails()}>
             <span> Load More </span>
             <FontAwesomeIcon icon={faChevronDown} />
           </p>
         )
       ) : null}
+
       <div className="space-container"></div>
     </>
   );
